0afbc245d32b5d1f00b16e53f51f966880b47c99
uwaifo <mr.uwaifo@gmail.com>
2020-09-02 09:57:14 +0100

    Connected Client to stripe api. Introduced enviroment variable to store secret key.

--- client.go
+++ client.go
@@ -1,20 +1,62 @@
 package stripeclient
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
 // Customer  . .
 type Customer struct {
 	ID string `json:"id"`
 }
 
 // Client . . .
-type Client struct{}
+type Client struct {
+	Key string `json:"key"`
+}
 
 // Customer . . .
 func (c *Client) Customer(token string) (*Customer, error) {
-	return nil, nil
+	endpoint := "https://api.stripe.com/v1/customers"
+	v := url.Values{}
+	v.Set("source", token)
+
+	request, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(v.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.SetBasicAuth(c.Key, "")
+
+	httpClient := http.Client{}
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	callBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(string(callBody))
+
+	var cus Customer
+	err = json.Unmarshal(callBody, &cus)
+	if err != nil {
+		return nil, err
+	}
+	return &cus, nil
 
 }
 
-/*
+/* ONE
 Th objective of this Client  is to query the stripe API for a Customers details
 There we do the folowing :
 1. Create a Customer struct to represent the response we are expecting from the call to the stripe API .
@@ -23,5 +65,28 @@ There we do the folowing :
 2. Create the Client struct to represent our own client instance that is making the API  call .
 
 3. cREATE THE CUTOMER method which
+
+*/
+
+/*TWO
+1.	Add stripe api endpont for creating a customer to the Customer method
+2.	Create/instatiate a variable to hold url.Values{} (standard library).
+ 	Using its Set() assign  it a "source"  as its key argument
+	and the corespoding value should be the token passed as arg to the Cutomer method
+3.	Make a http request object with earlier created value as parameters. Check for errors and return
+4.	Set header as app/json for the earlier created request object
+5.	Set basic authentication the reciever(Client in this case) Key, and password may be ""
+6.	Create a http client
+7.	Create a resp,err object based on the httpclient and use the Do() method
+	while passing the afor constructed request object and handle errors (just return nil,err)
+8.	Now defer res.Body.Close()
+9.	Using ioutil, read the response on=bjets Body while passing it to a newly created body object. Handle error
+10.	Stringify the body object and print it it statndard output
+11.	Declare a Customer varuale
+12.	***** . err = Unmarshal the body to the shape of the customer variable created ealier
+	(Here we are maping the body to the shape of the Customet struct) . Handle error
+13.	Finally return & reference of cutomer variable a,d nil
+
+Note: Read up on step 12
 
 */
--- client_test.go
+++ client_test.go
@@ -1,15 +1,26 @@
 package stripeclient_test
 
 import (
+	"log"
+	"os"
 	"strings"
 	"testing"
 
+	"github.com/joho/godotenv"
 	"github.com/uwaifo/stripeclient"
 )
 
-func TestClient_Customer(t *testing.T) {
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("no enviroment variable found")
+	}
 
-	c := stripeclient.Client{}
+}
+
+func TestClient_Customer(t *testing.T) {
+	apiKey := os.Getenv("STRIPE_SECRET_KEY")
+
+	c := stripeclient.Client{Key: apiKey}
 	tok := "tok_amex"
 	cus, err := c.Customer(tok)
 	if err != nil {
@@ -26,14 +37,10 @@ func TestClient_Customer(t *testing.T) {
 
 }
 
-/*
+/* Iterations One
+
 Here we create a standard test function with the testing package
 The idea is to attempt to call the stripe api to create a cutomer
-
-curl https://api.stripe.com/v1/customers \
-  -u sk_test_pflyokeRxblxT6MkGjzIEybT005avqxgI6: \
-  -d description="My First Test Customer (created for API docs)"
-
 
 
 1. Create an instance of the Client (struct created in my "strioe" package) c.
@@ -41,4 +48,12 @@ curl https://api.stripe.com/v1/customers \
 3. Also create a Customer object/instance and pass it our temp token as argument. Handle error
 4. If customer is nil, or in otherwords not validly created
 4. Check if the customer object we attempted to create is reponding with a json that has an id that is prefixed by "cus_"
+*/
+
+/* Iteration Two
+1. We add the API Key to instantiating of the Client{Key:"myapikey"}
+Note do not expose your secret api. Rather store in enviroment variables
+Note: I inroduced the init() function to load my enviromant variable using the godotenv package methods.
+
+
 */
